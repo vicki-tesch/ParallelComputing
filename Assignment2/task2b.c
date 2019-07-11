eca348f20c2f2da775d7b38729ca16cb385e2d48
David Lah <dlah@euler.wacc.wisc.edu>
2019-07-11 11:36:36 -0500

    task2b_complete

--- Assignment2/task2b.c
+++ Assignment2/task2b.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 #include "output.h"
-#include "output.c"
+
 
 int main() {
 	
 	int n; 
-	printf("Please insert an integer higher (or equal to) 0")
+	printf("Please insert an integer higher (or equal to) 0");
 	scanf("%d" , &n); 
 	
 	for (int i=0; i<=n; i++){
