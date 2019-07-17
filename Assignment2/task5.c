4c44e21ce91189a5eb158367d734b20654f16a78
David <davidlah_94@yahoo.de>
2019-07-17 11:37:41 +0200

    task5 accomplished on mac

--- Assignment2/task5.c
+++ Assignment2/task5.c
@@ -1,22 +1,36 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 #include "output.h"
 #include "output.c"
 
+int cmpfunc ( const void * a, const void * b) {
+    return ( *(int*)a - *(int*)b );
+}
+
+
+
 int main() {
 	
 	int n; 
-	printf("Please insert an integer between 1 and 99")
+    printf("Please insert an integer between 1 and 99 \n");
 	scanf("%d" , &n); 
-	malloc( sizeof(int)* (n+1)); 
-	
-	int myArray[n+1]
+//	(int *) malloc((n+1) * sizeof(int));
+    int *myArray;
+    myArray = (int *) malloc((n+1) * sizeof(int));
+ //   int myArray[n+1];
+    int i;
 	for (i=0; i<=n; i++){
-		myArray[i]= n-i;  
-		
+		myArray[i]= n-i;
+ //       printf("the %d value is %d\n",i,myArray[i]);
+
 	}
-	qsort (myArray,n+1,sizeof(int), cmpfunc)
+    qsort (myArray,n+1,sizeof(int), cmpfunc);
+    outputT5(myArray,n+1);
+    printf("\n");
+    free(myArray);
 	return 0; 
 }
 
 //TODO: 
+
---
+++ Assignment2/taskprintscan.h
@@ -0,0 +1,13 @@
+//
+//  taskprintscan.h
+//  
+//
+//  Created by David Lah on 17.07.19.
+//
+
+#ifndef taskprintscan_h
+#define taskprintscan_h
+
+#include <stdio.h>
+
+#endif /* taskprintscan_h */
